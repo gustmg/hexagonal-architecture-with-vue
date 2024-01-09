b1143ddbaf6aa1d1e9c49e720f063112b64c3a9f
Gustavo Mitre <gustavo.mitre.gallardo@gmail.com>
2024-01-09 15:30:05 -0600

    refactor(infrastructure): :recycle: Se elimina boilerplate de página principal

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,42 +1,15 @@
 <template>
-  <q-page class="row items-center justify-evenly">
-    <example-component
-      title="Example component"
-      active
-      :todos="todos"
-      :meta="meta"
-    ></example-component>
-  </q-page>
+  <BasePage>
+    <div class="row">
+      <div class="col-12">
+        <div class="text-h1">
+          Bienvenido
+        </div>
+      </div>
+    </div>
+  </BasePage>
 </template>
 
 <script setup lang="ts">
-import { Todo, Meta } from 'components/models';
-import ExampleComponent from 'components/ExampleComponent.vue';
-import { ref } from 'vue';
-
-const todos = ref<Todo[]>([
-  {
-    id: 1,
-    content: 'ct1',
-  },
-  {
-    id: 2,
-    content: 'ct2',
-  },
-  {
-    id: 3,
-    content: 'ct3',
-  },
-  {
-    id: 4,
-    content: 'ct4',
-  },
-  {
-    id: 5,
-    content: 'ct5',
-  },
-]);
-const meta = ref<Meta>({
-  totalCount: 1200,
-});
+import BasePage from 'src/components/base/BasePage.vue';
 </script>
