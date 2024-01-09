3a24db6530a9d326c37d15adddb9c9a8c95a5978
Gustavo Mitre <gustavo.mitre.gallardo@gmail.com>
2024-01-09 15:15:18 -0600

    feat(infrastructure): :sparkles: Se agregan rutas para página de inicio de sesión y registro de clientes

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -2,9 +2,28 @@ import { RouteRecordRaw } from 'vue-router';
 
 const routes: RouteRecordRaw[] = [
   {
+    path: '/login',
+    name: 'login',
+    component: () => import('src/pages/LoginPage.vue'),
+    meta: { requiresAuth: false },
+  },
+  {
+    path: '/register',
+    name: 'register',
+    component: () => import('src/pages/RegisterPage.vue'),
+    meta: { requiresAuth: false },
+  },
+  {
     path: '/',
     component: () => import('layouts/MainLayout.vue'),
-    children: [{ path: '', component: () => import('pages/IndexPage.vue') }],
+    children: [
+      {
+        name: 'home',
+        path: '',
+        component: () => import('pages/IndexPage.vue'),
+        meta: { requiresAuth: true },
+      },
+    ],
   },
 
   // Always leave this as last one,
