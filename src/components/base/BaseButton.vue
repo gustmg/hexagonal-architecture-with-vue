b15b300f532ef8f76e37aab33a012961bc1ec6f5
Gustavo Mitre <gustavo.mitre.gallardo@gmail.com>
2024-01-09 15:18:49 -0600

    feat(infrastructure): :sparkles: Se crea componente base para botones

---
+++ src/components/base/BaseButton.vue
@@ -0,0 +1,22 @@
+<template>
+  <q-btn
+    color="indigo"
+    padding="sm lg"
+    :label="label"
+    :outline="type === 'secondary'"
+  />
+</template>
+
+<script setup lang="ts">
+defineProps({
+  type: {
+    type: String,
+    default: 'primary',
+  },
+
+  label: {
+    type: String,
+    required: true,
+  },
+});
+</script>
