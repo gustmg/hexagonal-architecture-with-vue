634defa8f736f5ed42095b4e464f34222a14f046
Gustavo Mitre <gustavo.mitre.gallardo@gmail.com>
2024-01-09 15:29:30 -0600

    feat(infrastructure): :sparkles: Se crea página para registro de clientes

---
+++ src/pages/RegisterPage.vue
@@ -0,0 +1,16 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-page-container>
+      <BasePage class="row items-center justify-evenly">
+        <div class="col-3">
+          <CustomerRegistrationCard />
+        </div>
+      </BasePage>
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script setup lang="ts">
+import BasePage from 'src/components/base/BasePage.vue';
+import CustomerRegistrationCard from 'src/components/cards/CustomerRegistrationCard.vue';
+</script>
