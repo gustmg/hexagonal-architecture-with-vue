78f1ed7741fd21bc7ebdf49fdb936aa52106107d
Gustavo Mitre <gustavo.mitre.gallardo@gmail.com>
2024-01-09 15:24:21 -0600

    feat(infrastructure): :sparkles: Se crea componente para card de inicio de sesión

---
+++ src/components/cards/LoginCard.vue
@@ -0,0 +1,43 @@
+<template>
+  <BaseCard title="Inicio de sesión">
+    <template #body>
+      <LoginForm />
+    </template>
+    <template #actions>
+      <div class="col-6">
+        <BaseButton
+          label="Entrar"
+          class="full-width"
+          @click="login()"
+        />
+      </div>
+      <div class="col-12">
+        <q-separator class="q-my-lg" />
+      </div>
+      <div class="col-6">
+        <div class="text-center text-body2 q-pb-md">
+          ¿Aún no es usuario?
+        </div>
+        <BaseButton
+          label="Registrarse"
+          class="full-width"
+          type="secondary"
+          to="/register"
+        />
+      </div>
+    </template>
+  </BaseCard>
+</template>
+
+<script setup lang="ts">
+import { useCustomerStore } from 'src/stores/customer.store';
+import LoginForm from '../forms/LoginForm.vue';
+import BaseCard from '../base/BaseCard.vue';
+import BaseButton from '../base/BaseButton.vue';
+
+const customerStore = useCustomerStore();
+
+const login = async () => {
+  await customerStore.login();
+};
+</script>
