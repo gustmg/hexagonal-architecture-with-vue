ad5b96e804ed13583ca2fc34cd7e31f06e25f4fb
Gustavo Mitre <gustavo.mitre.gallardo@gmail.com>
2024-01-10 16:50:18 -0600

    feat(domain): :necktie: Se crean interfaz y clase para DTOs de clientes

--- src/modules/customer/domain/CustomerDto.ts
+++ src/modules/customer/domain/CustomerDto.ts
@@ -1,4 +1,9 @@
-export interface CustomerDto {
+import { v4 as uuidv4 } from 'uuid';
+import { ICustomerEntity } from './CustomerEntity';
+import { ICustomerRegistrationEntity } from './CustomerRegistrationEntity';
+import { ICustomerRegistrationDto } from './CustomerRegistrationDto';
+
+export interface ICustomerDto {
   id: string;
   formal_title: string;
   name: string;
@@ -8,16 +13,78 @@ export interface CustomerDto {
   email: string;
   state: string;
   city: string;
-  dealer: string;
-  interested_in: string;
-  required_info: string;
-  message: string;
-  privacy_advise: boolean;
 }
 
-export interface CustomerRegistrationDto {
+export class CustomerDto implements ICustomerDto {
+  id: string;
+
+  formal_title: string;
+
   name: string;
+
   last_name: string;
+
+  phone: string;
+
+  mobile_phone: string;
+
   email: string;
-  password: string;
+
+  state: string;
+
+  city: string;
+
+  constructor() {
+    this.id = '';
+    this.formal_title = '';
+    this.name = '';
+    this.last_name = '';
+    this.phone = '';
+    this.mobile_phone = '';
+    this.email = '';
+    this.state = '';
+    this.city = '';
+  }
+
+  fromCustomerEntity(customer: ICustomerEntity): this {
+    this.id = customer.id;
+    this.formal_title = customer.formalTitle;
+    this.name = customer.name;
+    this.last_name = customer.lastName;
+    this.phone = customer.phone;
+    this.mobile_phone = customer.mobilePhone;
+    this.email = customer.email;
+    this.state = customer.state;
+    this.city = customer.city;
+
+    return this;
+  }
+
+  fromCustomerRegistrationEntity(customerRegistrationEntity: ICustomerRegistrationEntity): this {
+    this.id = uuidv4();
+    this.formal_title = '';
+    this.name = customerRegistrationEntity.name;
+    this.last_name = customerRegistrationEntity.lastName;
+    this.phone = '';
+    this.mobile_phone = '';
+    this.email = customerRegistrationEntity.email;
+    this.state = '';
+    this.city = '';
+
+    return this;
+  }
+
+  fromCustomerRegistrationDto(customerRegistrationDto: ICustomerRegistrationDto): this {
+    this.id = uuidv4();
+    this.formal_title = '';
+    this.name = customerRegistrationDto.name;
+    this.last_name = customerRegistrationDto.last_name;
+    this.phone = '';
+    this.mobile_phone = '';
+    this.email = customerRegistrationDto.email;
+    this.state = '';
+    this.city = '';
+
+    return this;
+  }
 }
