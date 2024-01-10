a854267f9e87b832d5cccd6b2cafef69bfd164f5
Gustavo Mitre <gustavo.mitre.gallardo@gmail.com>
2024-01-10 17:59:00 -0600

    feat(infrastructure): :sparkles: Se crea componente para card de completado de información de clientes

---
+++ src/components/cards/CompleteCustomerInfoCard.vue
@@ -0,0 +1,29 @@
+<template>
+  <BaseCard title="Completa tu información">
+    <template #body>
+      <CompleteCustomerInfoForm />
+    </template>
+    <template #actions>
+      <div class="col-6">
+        <BaseButton
+          label="Guardar"
+          class="full-width"
+          @click="updateCustomer()"
+        />
+      </div>
+    </template>
+  </BaseCard>
+</template>
+
+<script setup lang="ts">
+import { useCustomerStore } from 'src/stores/customer.store';
+import BaseButton from '../base/BaseButton.vue';
+import BaseCard from '../base/BaseCard.vue';
+import CompleteCustomerInfoForm from '../forms/CompleteCustomerInfoForm.vue';
+
+const customerStore = useCustomerStore();
+
+const updateCustomer = () => {
+  customerStore.updateCustomer();
+};
+</script>
