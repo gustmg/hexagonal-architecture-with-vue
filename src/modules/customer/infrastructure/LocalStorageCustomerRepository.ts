30d45d9bde2ba4667467fc607d0badf3a63a1f8b
Gustavo Mitre <gustavo.mitre.gallardo@gmail.com>
2024-01-10 17:37:00 -0600

    feat(infrastructure): :sparkles: Se actualizan types y valores de retorno en repositorio de local storage para clientes

--- src/modules/customer/infrastructure/LocalStorageCustomerRepository.ts
+++ src/modules/customer/infrastructure/LocalStorageCustomerRepository.ts
@@ -1,19 +1,19 @@
-import { v4 as uuidv4 } from 'uuid';
-import { CustomerDto, CustomerRegistrationDto } from '../domain/CustomerDto';
-import { CustomerLoginForm } from '../domain/CustomerLoginForm';
+import { ICustomerLogin } from '../domain/CustomerLoginEntity';
 import { CustomerRepository } from '../domain/CustomerRepository';
+import { CustomerDto, ICustomerDto } from '../domain/CustomerDto';
+import { ICustomerRegistrationDto } from '../domain/CustomerRegistrationDto';
 
-function getAllCustomersFromLocalStorage():Map<string, CustomerDto> {
+function getAllCustomersFromLocalStorage():Map<string, ICustomerDto> {
   const customers = localStorage.getItem('customers');
 
   if (!customers) {
     return new Map();
   }
 
-  return new Map(JSON.parse(customers) as Iterable<[string, CustomerDto]>);
+  return new Map(JSON.parse(customers) as Iterable<[string, ICustomerDto]>);
 }
 
-async function login(loginForm: CustomerLoginForm): Promise<CustomerDto> {
+async function login(loginForm: ICustomerLogin): Promise<ICustomerDto> {
   const customers = getAllCustomersFromLocalStorage();
   const customer = customers.get(loginForm.email);
 
@@ -24,25 +24,23 @@ async function login(loginForm: CustomerLoginForm): Promise<CustomerDto> {
   return Promise.resolve(customer);
 }
 
-function register(registerForm: CustomerRegistrationDto): Promise<void> {
+function register(registerForm: ICustomerRegistrationDto): Promise<void> {
   const customers = getAllCustomersFromLocalStorage();
 
-  customers.set(registerForm.email, {
-    id: uuidv4(),
-    formal_title: '',
-    name: registerForm.name,
-    last_name: registerForm.last_name,
-    phone: '',
-    mobile_phone: '',
-    email: registerForm.email,
-    state: '',
-    city: '',
-    dealer: '',
-    interested_in: '',
-    required_info: '',
-    message: '',
-    privacy_advise: false,
-  });
+  customers.set(
+    registerForm.email,
+    new CustomerDto().fromCustomerRegistrationDto(registerForm),
+  );
+
+  localStorage.setItem('customers', JSON.stringify(Array.from(customers.entries())));
+
+  return Promise.resolve();
+}
+
+function update(customer: CustomerDto): Promise<void> {
+  const customers = getAllCustomersFromLocalStorage();
+
+  customers.set(customer.email, customer);
 
   localStorage.setItem('customers', JSON.stringify(Array.from(customers.entries())));
 
@@ -53,5 +51,6 @@ export default function createLocalStorageCustomerRepository(): CustomerReposito
   return {
     login,
     register,
+    update,
   };
 }
