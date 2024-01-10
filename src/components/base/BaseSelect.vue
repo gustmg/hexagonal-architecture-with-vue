d0243353e54d8c1899ef3141c214f55e875983dc
Gustavo Mitre <gustavo.mitre.gallardo@gmail.com>
2024-01-10 17:52:53 -0600

    feat(infrastructure): :sparkles: Se crea componente base para inputs de selección

---
+++ src/components/base/BaseSelect.vue
@@ -0,0 +1,20 @@
+<template>
+  <q-select
+    :model-value="modelValue"
+    outlined
+    map-options
+    emit-value
+    @update:model-value="$emit('update:modelValue', $event)"
+  />
+</template>
+
+<script setup>
+defineProps({
+  modelValue: {
+    type: [String, Number, Array, Object],
+    default: '',
+  },
+});
+
+defineEmits(['update:modelValue']);
+</script>
