e785999d029aa8ea706c10c182ced24ea8cdfa27
Gustavo Mitre <gustavo.mitre.gallardo@gmail.com>
2024-01-10 18:00:04 -0600

    feat(infrastructure): :sparkles: Se visualiza card para completar información de clientes en página principal

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,15 +1,15 @@
 <template>
   <BasePage>
-    <div class="row">
-      <div class="col-12">
-        <div class="text-h1">
-          Bienvenido
-        </div>
+    <BaseRow>
+      <div class="col-3">
+        <CompleteCustomerInfoCard />
       </div>
-    </div>
+    </BaseRow>
   </BasePage>
 </template>
 
 <script setup lang="ts">
 import BasePage from 'src/components/base/BasePage.vue';
+import BaseRow from 'src/components/base/BaseRow.vue';
+import CompleteCustomerInfoCard from 'src/components/cards/CompleteCustomerInfoCard.vue';
 </script>
