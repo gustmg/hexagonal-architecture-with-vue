56b63b1b0d716f230478853662b22a6adef1f381
Gustavo Mitre <gustavo.mitre.gallardo@gmail.com>
2024-01-13 16:11:22 -0600

    feat(infrastructure): :sparkles: Se crea componente para card de vehículo a agregar

---
+++ src/components/cards/AddCustomerVehicleCard.vue
@@ -0,0 +1,24 @@
+<template>
+  <BaseCard title="Agregar un vehículo a mi lista">
+    <template #body>
+      <AddCustomerVehicleForm />
+      <q-separator class="q-my-md" />
+      <AddCustomerVehicleTotalSection />
+    </template>
+    <template #actions>
+      <div class="col-6">
+        <BaseButton
+          label="Agregar"
+          class="full-width"
+        />
+      </div>
+    </template>
+  </BaseCard>
+</template>
+
+<script setup lang="ts">
+import BaseButton from '../base/BaseButton.vue';
+import BaseCard from '../base/BaseCard.vue';
+import AddCustomerVehicleForm from '../forms/AddCustomerVehicleForm.vue';
+import AddCustomerVehicleTotalSection from './AddCustomerVehicleTotalSection.vue';
+</script>
