08ddc8969297821ced7adf182c7f74bcafedcd94
Gustavo Mitre <gustavo.mitre.gallardo@gmail.com>
2024-01-13 16:18:14 -0600

    feat(infrastructure): :sparkles: Se crea componente para list item de vehículos

---
+++ src/components/list-items/VehicleListItem.vue
@@ -0,0 +1,30 @@
+<template>
+  <q-item>
+    <q-item-section avatar>
+      <q-img
+        :src="vehicle.img"
+        alt="img"
+        width="80px"
+        height="80px"
+        fit="contain"
+      />
+    </q-item-section>
+    <q-item-section class="text-caption">
+      <q-item-label>{{ vehicle.model }}</q-item-label>
+      <q-item-label>{{ vehicle.year }}</q-item-label>
+      <q-item-label>{{ vehicle.color }}</q-item-label>
+    </q-item-section>
+    <q-item-section side>
+      <q-item-label>${{ vehicle.price }}</q-item-label>
+    </q-item-section>
+  </q-item>
+</template>
+
+<script setup>
+defineProps({
+  vehicle: {
+    type: Object,
+    required: true,
+  },
+});
+</script>
