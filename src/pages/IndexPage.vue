59f333380329810592042cf994baa1379c83a94e
Gustavo Mitre <gustavo.mitre.gallardo@gmail.com>
2024-01-13 16:13:22 -0600

    feat(infrastructure): :sparkles: Se visualiza card para agregar vehículos
    La card para agregar vehículos se visualizará hasta que el cliente tenga todos sus datos completos.

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -2,7 +2,8 @@
   <BasePage>
     <BaseRow>
       <div class="col-3">
-        <CompleteCustomerInfoCard />
+        <CompleteCustomerInfoCard v-if="!customerStore.isCustomerWithCompleteData" />
+        <AddCustomerVehicleCard v-else />
       </div>
     </BaseRow>
   </BasePage>
@@ -11,5 +12,16 @@
 <script setup lang="ts">
 import BasePage from 'src/components/base/BasePage.vue';
 import BaseRow from 'src/components/base/BaseRow.vue';
+import AddCustomerVehicleCard from 'src/components/cards/AddCustomerVehicleCard.vue';
 import CompleteCustomerInfoCard from 'src/components/cards/CompleteCustomerInfoCard.vue';
+import { useCustomerStore } from 'src/stores/customer.store';
+import { useVehicleStore } from 'src/stores/vehicle.store';
+import { onMounted } from 'vue';
+
+const customerStore = useCustomerStore();
+const vehicleStore = useVehicleStore();
+
+onMounted(async () => {
+  await vehicleStore.fetchVehicles();
+});
 </script>
