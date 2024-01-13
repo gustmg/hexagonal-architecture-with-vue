8dfb91960ea971d4632e918496c5d49b3f0b5ac9
Gustavo Mitre <gustavo.mitre.gallardo@gmail.com>
2024-01-13 16:07:32 -0600

    feat(infrastructure): :sparkles: Se habilita slot para items personalizados en inputs de selección

--- src/components/base/BaseSelect.vue
+++ src/components/base/BaseSelect.vue
@@ -5,14 +5,29 @@
     map-options
     emit-value
     @update:model-value="$emit('update:modelValue', $event)"
-  />
+  >
+    <template
+      v-if="customOption"
+      #option="scope"
+    >
+      <slot
+        name="option"
+        :props="scope"
+      />
+    </template>
+  </q-select>
 </template>
 
 <script setup>
 defineProps({
   modelValue: {
-    type: [String, Number, Array, Object],
+    type: [String, Number, Array, Object, null],
     default: '',
+  },
+
+  customOption: {
+    type: Boolean,
+    default: false,
   },
 });
 
