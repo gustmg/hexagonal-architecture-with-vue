8bdc951f50a3bee90e92dd2b18dbfb3ff9114167
Gustavo Mitre <gustavo.mitre.gallardo@gmail.com>
2024-01-13 16:19:09 -0600

    refactor(infrastructure): :recycle: Se utiliza componente para visualizar list items en selector de vehículos

--- src/components/forms/AddCustomerVehicleForm.vue
+++ src/components/forms/AddCustomerVehicleForm.vue
@@ -11,28 +11,11 @@
           custom-option
         >
           <template #option="{props}">
-            <q-item
+            <VehicleListItem
+              :vehicle="props.opt"
               clickable
               @click="vehicleStore.addVehicleForm.vehicleId = props.opt.id"
-            >
-              <q-item-section avatar>
-                <q-img
-                  :src="props.opt.img"
-                  alt="img"
-                  width="80px"
-                  height="80px"
-                  fit="contain"
-                />
-              </q-item-section>
-              <q-item-section class="text-caption">
-                <q-item-label>{{ props.opt.model }}</q-item-label>
-                <q-item-label>{{ props.opt.year }}</q-item-label>
-                <q-item-label>{{ props.opt.color }}</q-item-label>
-              </q-item-section>
-              <q-item-section side>
-                <q-item-label>${{ props.opt.price }}</q-item-label>
-              </q-item-section>
-            </q-item>
+            />
           </template>
         </BaseSelect>
       </div>
@@ -55,6 +38,7 @@ import { useVehicleStore } from 'src/stores/vehicle.store';
 import BaseRow from '../base/BaseRow.vue';
 import BaseSelect from '../base/BaseSelect.vue';
 import BaseCheckbox from '../base/BaseCheckbox.vue';
+import VehicleListItem from '../list-items/VehicleListItem.vue';
 
 const vehicleStore = useVehicleStore();
 </script>
